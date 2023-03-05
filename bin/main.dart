da18179113ac7865a1e897b30eb1b44c62c59463
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-03-05 13:42:56 -0300

    optimiznado codigo

--- bin/main.dart
+++ bin/main.dart
@@ -1,20 +1,7 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
+// import 'dart:convert';
 
 import 'package:paquetes/paquetes.dart' as paquetes;
 
 void main(List<String> arguments) async {
-  final url = Uri.parse('https://reqres.in/api/users?page=2');
-  // final url = Uri.https('reqres.in', 'api/users?page=2');
-  // final response = await http.get(url);
-  // print(jsonDecode(response.body));
-
-  http.get(url).then((res) {
-    // print(res.statusCode);
-    final body = jsonDecode(res.body);
-    print('page: ${body['page']}');
-    print('per_page:  ${body['per_page']}');
-    print('id del tercer elemento:  ${body['data'][2]['id']}');
-  });
+  paquetes.getReqRes_service();
 }
--- lib/paquetes.dart
+++ lib/paquetes.dart
@@ -1,1 +1,23 @@
+import 'package:http/http.dart' as http;
 
+import 'package:paquetes/classes/reqres_respuesta.dart';
+
+void getReqRes_service() {
+  final url = Uri.parse('https://reqres.in/api/users?page=2');
+  // final url = Uri.https('reqres.in', 'api/users?page=2');
+  // final response = await http.get(url);
+  // print(jsonDecode(response.body));
+
+  http.get(url).then((res) {
+    final resReqRes = reqResRespuestaFromJson(res.body);
+    // print(res.statusCode);
+    // final body = jsonDecode(res.body);
+    // print('page: ${body['page']}');
+    // print('per_page:  ${body['per_page']}');
+    // print('id del tercer elemento:  ${body['data'][2]['id']}');
+
+    print('page: ${resReqRes.page}');
+    print('per_page:  ${resReqRes.perPage}');
+    print('id del tercer elemento:  ${resReqRes.data[2].id}');
+  });
+}
